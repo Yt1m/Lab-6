9f590b6c4b3bf63d41c71ea0ef6424711d65771c
Yt1m <144239923+Yt1m@users.noreply.github.com>
2023-12-26 11:22:09 +0200

    Add files via upload

---
+++ 3.1-базовый на4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x, r;
+
+        while (true)
+        {
+            Console.Write("Введите значение x: ");
+            if (!double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("Ошибка");
+                continue;
+            }
+
+            if (0 < x && x < 3.14)
+            {
+                r = Math.Sin(x);
+            }
+            else if (-3.14 <= x && x < 0)
+            {
+                r = 3 * x;
+            }
+            else if (x < -2 * 3.14 || x > 2 * 3.14)
+            {
+                r = 2 * x;
+            }
+            else
+            {
+                r = 0;
+            }
+
+            Console.WriteLine($"При x = {x}, r = {r}");
+        }
+    }
+}
